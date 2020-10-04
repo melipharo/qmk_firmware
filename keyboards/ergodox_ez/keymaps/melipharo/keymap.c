21be4dc88dfb5ff74943c0730bf043c2b605748a
melipharo <sirMelipharo@gmail.com>
2020-10-04 19:42:55 +0700

    typo fix

--- keyboards/ergodox_ez/keymaps/melipharo/keymap.c
+++ keyboards/ergodox_ez/keymaps/melipharo/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_DEL,         KC_Q,                KC_W,       KC_E,       KC_R,       KC_T,       TG(FN_NUM),                 TG(FN_NUM),         KC_Y,       KC_U,       KC_I,       KC_O,           KC_P,                   KC_BSLS,
         KC_BSPC,        KC_A,                KC_S,       KC_D,       KC_F,       KC_G,                                                       KC_H,       KC_J,       KC_K,       KC_L,           KC_SCLN,                KC_ENT,
         KC_LSFT,        KC_Z,                KC_X,       KC_C,       KC_V,       KC_B,       LSA(KC_1),                  LSA(KC_2),          KC_N,       KC_M,       KC_COMM,    KC_DOT,         LT(SYSLEDS, KC_SLSH),   KC_RSFT,
-        KC_LCTRL,       LT(FN_NUM, KC_GRV),  KC_QUOT,    KC_LEFT,    KC_RGHT,                                                                            KC_UP,      KC_DOWN,    KC_LBRC,        LT(FN_NUM, KC_RBRC),    LT(MEDIA, KC_NO)
+        KC_LCTRL,       LT(FN_NUM, KC_GRV),  KC_QUOT,    KC_LEFT,    KC_RGHT,                                                                            KC_UP,      KC_DOWN,    KC_LBRC,        LT(FN_NUM, KC_RBRC),    LT(MEDIA, KC_NO),
                                                                                 KC_LALT,  KC_ESC,                      KC_PAUS,    LALT_T(KC_APP),
                                                                                                     KC_HOME,           KC_PGUP,
                                                                                 KC_SPC,   KC_TAB,   KC_END,            KC_PGDN,    KC_LGUI,    KC_ENT
